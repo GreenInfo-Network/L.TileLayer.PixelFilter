215b49eb1c3fd2f205e05c174457c5fca7971fd7
tsinn <timothysinnott@hotmail.com>
2018-10-29 15:10:49 -0400

    Update tiles on demo3.js
    #14

--- demo3.js
+++ demo3.js
@@ -6,7 +6,7 @@ function init() {
     }).setView([39.8283,-98.5795],3);
 
     // the PixelFilter tilelayer
-    OVERLAY = L.tileLayerPixelFilter('tiles/{z}/{x}/{y}.png', {
+    OVERLAY = L.tileLayerPixelFilter('https://storage.googleapis.com/ee-layers/srtm/{z}/{x}/{y}.png', {
         tms: true, // I used gdal2tiles.py so these tiles use TMS numbering, not WMS-c numbering
         matchRGBA: null, // preserve whatever color was in the pixel previously
         missRGBA:  [ 255, 255, 255, 255 ], // fill non-matching pixels with solid white
