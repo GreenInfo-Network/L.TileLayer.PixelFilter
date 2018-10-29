2da323e823e94a455a1ba9f0ec2a5a8743fc599f
tsinn <timothysinnott@hotmail.com>
2018-10-29 15:16:49 -0400

    Update to demo3.js
    #14

--- demo3.js
+++ demo3.js
@@ -3,12 +3,12 @@ function init() {
     var MAP = L.map('map', {
         minZoom:1,
         maxZoom:8,
-    }).setView([39.8283,-98.5795],3);
+    }).setView([39.8283,-98.5795],4);
 
     // the PixelFilter tilelayer
-    OVERLAY = L.tileLayerPixelFilter('https://storage.googleapis.com/ee-layers/srtm/{z}/{x}/{y}.png', {
-        tms: true, // I used gdal2tiles.py so these tiles use TMS numbering, not WMS-c numbering
+    OVERLAY = L.tileLayerPixelFilter('https://storage.googleapis.com/ee-layers/srtm/{z}/{x}/{y}', {
+        tms: false, // I used gdal2tiles.py so these tiles use TMS numbering, not WMS-c numbering
         matchRGBA: null, // preserve whatever color was in the pixel previously
-        missRGBA:  [ 255, 255, 255, 255 ], // fill non-matching pixels with solid white
+        missRGBA:  [ 255, 255, 255, 0 ], // fill non-matching pixels with solid white
     }).addTo(MAP);
 }
