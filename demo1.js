3651673c4cc95cce32ae61333f52ce33a402f4f4
Greg Allensworth <gregor@greeninfo.org>
2026-04-06 08:21:44 -0700

    fix demos to work with Leaflet 1.9, remove demo3

--- demo1.js
+++ demo1.js
@@ -5,8 +5,8 @@ function init() {
         maxZoom:3,
     }).setView([0,0],1);
 
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
     }).addTo(MAP);
 
     // the PixelFilter tilelayer
